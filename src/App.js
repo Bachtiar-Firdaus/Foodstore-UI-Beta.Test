2a3142034b9423df2ab1ff0606e22660731f4f81
rluciver <rluciver@aol.com>
2021-01-08 03:58:07 +0700

    Menyimpan Atate Auth ke Local Storage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Provider } from "react-redux";
@@ -6,7 +7,13 @@ import "upkit/dist/style.min.css";
 import store from "./app/store";
 import Home from "./pages/Home";
 
+import { listen } from "./app/listener";
+
 function App() {
+  // panggil fungsi listen() sekali saja saat komponen selesai render pertama kali
+  React.useEffect(() => {
+    listen();
+  }, []);
   return (
     <Provider store={store}>
       <Router>
---
+++ src/app/listener.js
@@ -0,0 +1,18 @@
+import store from "./store";
+
+let currentAuth;
+function listener() {
+  // buat variabel previousAuth dan berikan currentAuth sebagai nilai
+  let previousAuth = currentAuth;
+  //  update nilai currentAuth dari nilai state terbaru
+  currentAuth = store.getState().auth;
+  // cek apakah nilai state `auth` berubah dari nilai sebelumnya
+  if (currentAuth !== previousAuth) {
+    // jika berubah simpan ke localStorage
+    localStorage.setItem("auth", JSON.stringify(currentAuth));
+  }
+}
+function listen() {
+  store.subscribe(listen);
+}
+export { listen };

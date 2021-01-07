78990cc66be2464e84d8305bad8391ce779207d7
rluciver <rluciver@aol.com>
2021-01-08 03:24:19 +0700

    Membungkus Kode Aplikasi dengan <Provider>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Provider } from "react-redux";
 import "upkit/dist/style.min.css";
+
+import store from "./app/store";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <Router>
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
       </Router>
-    </div>
+    </Provider>
   );
 }
 

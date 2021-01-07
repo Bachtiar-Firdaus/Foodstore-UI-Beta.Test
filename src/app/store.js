c3e762f3b96aad3358f717fd500048a35ae0d368
rluciver <rluciver@aol.com>
2021-01-08 03:45:15 +0700

    Mendaftarkan Reducer Auth ke Store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,14 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 //import redux-thunk middleware
 import thunk from "redux-thunk";
+
+import authReducer from "../features/Auth/reducer";
 //composer enhancer menghubungkan dengan Chrome DevTools Redux
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //gabung reducer
-const rootReducers = combineReducers({});
+const rootReducers = combineReducers({
+  auth: authReducer,
+});
 //buat store, dan gunakan composerEnchancer + middleware thunk
 const store = createStore(
   rootReducers,

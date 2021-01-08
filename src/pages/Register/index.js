e59b7411c41596b896876c27592550948b6b632f
rluciver <rluciver@aol.com>
2021-01-08 11:44:48 +0700

    Validasi Input Form dengan react-hook-form<i>~3</i>

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -4,6 +4,8 @@ import { Button, Card, FormControl, InputText, LayoutOne } from "upkit";
 
 import { useForm } from "react-hook-form";
 
+import { rules } from "./validation";
+
 export default function Register() {
   let { register, handleSubmit, errors, setError } = useForm();
   const onSubmit = async (FormData) => {
@@ -13,36 +15,42 @@ export default function Register() {
   return (
     <LayoutOne size="small">
       <Card color="white">
-        <form on onSubmit={handleSubmit(onSubmit)}>
-          <FormControl>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl errorMessage={errors.full_name?.message}>
             <InputText
               name="full_name"
               placeholder="Nama Lengkap"
               fitContainer
-              ref={register}
+              ref={register(rules.full_name)}
             />
+          </FormControl>
+          <FormControl errorMessage={errors.email?.message}>
             <InputText
               name="email"
               placeholder="Email"
               fitContainer
-              ref={register}
+              ref={register(rules.email)}
             />
+          </FormControl>
+          <FormControl errorMessage={errors.password?.message}>
             <InputText
               name="password"
               placeholder="Password"
               fitContainer
-              ref={register}
+              ref={register(rules.password)}
             />
+          </FormControl>
+          <FormControl errorMessage={errors.password_confirmation?.message}>
             <InputText
               name="password_confirmation"
               placeholder="Konfirmasi Password"
               fitContainer
-              ref={register}
+              ref={register(rules.password_confirmation)}
             />
-            <Button size="large" fitContainer>
-              Mendaftar
-            </Button>
           </FormControl>
+          <Button size="large" fitContainer>
+            Mendaftar
+          </Button>
         </form>
       </Card>
     </LayoutOne>
